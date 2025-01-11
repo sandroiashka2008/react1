5a38b4d917166347b0ae4c4da0d20bb139e33616
sandro antadze <sandroiashvili169@gmail.com>
2025-01-12 00:27:08 +0400

    reactdavaleba

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
+import React from 'react';
 import './App.css'
 
-function App() {
-  
 
+function App() {
   return (
-    <div>
-   <h1>hello world </h1>
-   <h1 id='h2'>hello</h1>
-   </div>
-  )
+    <div className="App">
+      <h1>Hello World</h1> 
+    </div>
+  );
 }
 
-export default App
+export default App;
